e3bfd00856c6496bbf8e5617d4b4d2f4f46a0b11
Hector <rootdevelopper>
2016-06-09 06:02:47 -0500

    initial physics set up
    set up paddle, ball and starting the walls

---
+++ Block_breaker/Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print ("Trigger");
+		levelManager.LoadLevel("Win Screen");
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		print ("Collision");	
+	}
+	
+}
